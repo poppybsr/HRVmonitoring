29eb2fdab59403e757b8013f7b06df54736042a3
poppybsr <poppy.bt120@gmail.com>
2020-02-04 14:44:17 +0700

    first commit
    get data from firebase

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 const database = firebase.database();
 const messageRef = database.ref("sensor");
+// const ageRef = database.ref("age")
+// const arrMax = arr => Math.max(times);
+
+
 new Vue({
     el:"#age",
     data:{
-        age:Int16Array,
+        age:'',
         time:'',
         times:[],
+        allTime:[],
         valueECG:'',
     },
     methods:{
         storeMessage:function(){
             messageRef.push({time:this.time,value:this.valueECG})
             this.time=''
-
         }
     },
+    
     created(){
         // show data from database
         messageRef.on('child_added' ,snapshot=>{
             // push data from database to array
             this.times.push(snapshot.val())
-            console.log(snapshot.val());
         })
-    }
 
-    
+        console.log(this.times)
+    },
+        
+
 })
