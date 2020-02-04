25b42a4540114d3a48927e8b82ac6e2aad0dcab7
poppybsr <poppy.bt120@gmail.com>
2020-02-04 20:03:20 +0700

    calculate func
    calculate HRV

--- app.js
+++ app.js
@@ -4,34 +4,53 @@ const messageRef = database.ref("sensor");
 new Vue({
     el: "#age",
     data: {
-        age: '',
-        times: [],
-        allTime: [],
-        valueECG: '',
-        arr: [],
-        finalArray: [],
-        sum: 0
+        // age:'',
+        values: [],
+        sum:'',
+        ageInput: 0,
+        minus:'',
+        power2:'',
+        total:'',
+        square:'',
+        count:'',
+
+        
     },
     methods: {
         storeMessage: function () {
-            messageRef.push({ time: this.time, value: this.valueECG })
-            this.time = ''
+            messageRef.push({ time: this.ageInput})
+            this.ageInput = ''
+        },
+
+        calculate:function(){
+            this.minus = 0
+            this.sum = 0
+            this.power2 = 0
+            this.total = 0
+            this.square = 0
+            this.count = this.values.length - 1
+ 
+            for (var i=0; i< this.count; i++){
+                this.minus = this.values[i+1] - this.values[i]      //RR1-RR0
+                this.power2 = Math.pow(this.minus,2)                //(RR1-RR0)^2
+                this.sum += this.power2                             //summation
+            }
+                this.square = Math.sqrt(this.sum)                   //square
+                this.total = this.square/this.count
+            //console.log(this.minus)
         }
     },
 
     created() {
         // show data from database
-        
-
         messageRef.on('child_added', snapshot => {
             // push data from database to array
-            this.times.push(snapshot.val().time)
-
-            this.sum += snapshot.val().time
+            this.values.push(snapshot.val().time)
+            // this.sum += snapshot.val().time
+            // console.log(this.sum)
+            this.calculate();
         })
-        
-
-
+        // this.calC();
     },
 
 })
