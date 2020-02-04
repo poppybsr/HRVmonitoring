510d68b2c3e457fc9264c793db3a21af811bd30d
poppybsr <poppy.bt120@gmail.com>
2020-02-04 16:16:54 +0700

    obj array get
    sum data time

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 const database = firebase.database();
 const messageRef = database.ref("sensor");
-// const ageRef = database.ref("age")
-// const arrMax = arr => Math.max(times);
-
 
 new Vue({
-    el:"#age",
-    data:{
-        age:'',
-        time:'',
-        times:[],
-        allTime:[],
-        valueECG:'',
+    el: "#age",
+    data: {
+        age: '',
+        times: [],
+        allTime: [],
+        valueECG: '',
+        arr: [],
+        finalArray: [],
+        sum: 0
     },
-    methods:{
-        storeMessage:function(){
-            messageRef.push({time:this.time,value:this.valueECG})
-            this.time=''
+    methods: {
+        storeMessage: function () {
+            messageRef.push({ time: this.time, value: this.valueECG })
+            this.time = ''
         }
     },
-    
-    created(){
-        // show data from database
-        messageRef.on('child_added' ,snapshot=>{
-            // push data from database to array
-            this.times.push(snapshot.val())
-        })
 
-        console.log(this.times)
-    },
+    created() {
+        // show data from database
         
+
+        messageRef.on('child_added', snapshot => {
+            // push data from database to array
+            this.times.push(snapshot.val().time)
+
+            this.sum += snapshot.val().time
+        })
+        
+
+
+    },
 
 })
