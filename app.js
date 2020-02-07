ae56f0e8401a130c781a478a679c6a7609224a43
poppybsr <poppy.bt120@gmail.com>
2020-02-07 16:04:31 +0700

    New calculate with bootstrap
    cal RMSSD with find peak value

--- app.js
+++ app.js
@@ -2,56 +2,85 @@ const database = firebase.database();
 const messageRef = database.ref("sensor");
 
 new Vue({
-    el: "#age",
-    data: {
-        // age:'',
-        values: [],
+    el:"#age",
+    data:{
+        age:'',
+        values:[],
+        times:[],
+        allTime:[],
+        valueECG:'',
+        minus:[],
+        RR:[],
         sum:'',
-        ageInput: 0,
-        minus:'',
-        power2:'',
-        total:'',
-        square:'',
         count:'',
-
+        power2:[],
+        total:'',
+        mean:'',
+        maxValue:0,
+        maxTime:0,
+        arrMax:[],
+        countDrop:1
+    },
+    methods:{
+        storeMessage:function(){
+            messageRef.push({time:this.time,value:this.valueECG})
+            this.time=''
+        },
+        calculate:function(){
+            this.sum = 0
+            this.mean = 0
+            this.square = 0
+            this.count = this.arrMax.length - 1
+            // console.log(this.arrMax)
+            for (var i=0; i<this.count; i++){
+                this.minus[i] = this.arrMax[i+1][1] - this.arrMax[i][1]      //time2-time1                   
+            }
+            for(var i=0;i<this.count-1;i++){
+                this.RR[i] = this.minus[i+1] - this.minus[i]    //RR2-RR1
+            }
+            for(var i=0;i<this.count-1;i++){
+                this.power2[i] = this.RR[i]*this.RR[i]              //(RR2-RR1)^2
+            }
+            for(var i=0;i<this.count-1;i++){
+                // console.log(this.power2)                        
+                this.sum += this.power2[i]                          //sum diff RRinterval
+            }
+                this.mean = this.sum/this.count
+                this.total = Math.sqrt(this.mean)                   //square
+                this.total = this.total.toFixed(2)                  //fix dot number
+            //console.log(this.minus)
+        }
         
     },
-    methods: {
-        storeMessage: function () {
-            messageRef.push({ time: this.ageInput})
-            this.ageInput = ''
-        },
-
-        calculate:function(){
-            this.minus = 0
-            this.sum = 0
-            this.power2 = 0
-            this.total = 0
-            this.square = 0
-            this.count = this.values.length - 1
- 
-            for (var i=0; i< this.count; i++){
-                this.minus = this.values[i+1] - this.values[i]      //RR1-RR0
-                this.power2 = Math.pow(this.minus,2)                //(RR1-RR0)^2
-                this.sum += this.power2                             //summation
-            }
-                this.square = Math.sqrt(this.sum)                   //square
-                this.total = this.square/this.count
-                this.total = parseFloat(this.total).toFixed(2);
-            //console.log(this.minus)
-        },
-    },
-
-    created() {
+    
+    created(){
         // show data from database
-        messageRef.on('child_added', snapshot => {
+        messageRef.on('child_added' ,snapshot=>{
             // push data from database to array
-            this.values.push(snapshot.val().time)
-            // this.sum += snapshot.val().time
-            // console.log(this.sum)
-            this.calculate();
-        })
-        // this.calC();
-    },
+            this.values.push(snapshot.val().value)
+            this.times.push(snapshot.val().time)
+            //console.log(snapshot.val().time); 
 
+            if(this.maxValue <  snapshot.val().value){
+                this.maxValue = snapshot.val().value;
+                this.maxTime = snapshot.val().time;
+                // this.countDrop=0;
+                // console.log("value Increase")
+                this.countDrop=1
+            }else{
+                // console.log("value Drop")
+                this.countDrop--
+            }
+            if(this.countDrop==0){
+                var tempData = [];
+                tempData[0] = this.maxValue;
+                tempData[1] = this.maxTime;
+                this.arrMax.push(tempData)
+                this.maxValue = snapshot.val().value;
+                this.maxTime = snapshot.val().time;
+                this.countDrop--;
+            }
+            this.calculate(); 
+        }) 
+    },
 })
