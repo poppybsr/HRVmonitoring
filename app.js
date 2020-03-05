c606cb9f0b26e594f1015ad7a972393013019809
poppybsr <poppy.bt120@gmail.com>
2020-03-06 01:12:39 +0700

    calculate heart rate
    1.start calculate heart rate
    2.manage unused variables

--- app.js
+++ app.js
@@ -1,42 +1,49 @@
 const database = firebase.database();
 const messageRef = database.ref("allData");
-const messageRef2 = database.ref("allData");
+// const messageRef2 = database.ref("allData");
 
 new Vue({
     el:"#webhrv",
     data:{
-        age:'',
-        values:[],
+        // age:'',
+//snapshot data
+        values:[], 
         times:[],
         values2:[],
         times2:[],
-        allTime:[],
-        valueECG:'',
-        minus:[],
-        minus2:[],
+        // allTime:[],
+        // valueECG:'',
+// calculate
+        minus:[], 
+        // minus2:[],
         RR:[],
         sum:'',
         count:'',
         power2:[],
         total:'',
         mean:'',
-        maxValue:0,
-        maxTime:0,
-        arrMax:[],
-        countDrop:1,
+//for check drop
+        // maxValue:0, 
+        // maxTime:0,
+        // arrMax:[],
+        // countDrop:1,
+// filter function
         dataFilter:[], //all data after filter
         pushValue:'', //check value and push minus
         arrFilter:[], //push all data to arr
         arrTimeFilter:[],
         baseLine:'',
-        baseLine2:'',
+        avgBaseline:'',
+// heartBeat function
+        diffTime:'',
+        heartRate:'',
 
     },
     methods:{
-        storeMessage:function(){
-            messageRef.push({time:this.time,value:this.valueECG})
-            this.time=''
-        },
+        // storeMessage:function(){
+        //     messageRef.push({time:this.time,value:this.valueECG})
+        //     this.time=''
+        // },
 
         filter:function(){
             for (var i=0; i<this.values.length; i++){
@@ -45,25 +52,26 @@ new Vue({
             }
             // console.log(this.arrTimeFilter)
 
-            // cal baseLine for filter data
+            // calculate(avg data) baseLine for filter data
             this.baseLine=0
-            this.baseLine2=0
+            this.avgBaseline=0
             for (var i=0; i<this.arrFilter.length; i++){
                         this.baseLine+=this.arrFilter[i]
             }
-            this.baseLine2=this.baseLine/this.arrFilter.length
-            console.log(this.baseLine2)
+            this.avgBaseline=this.baseLine/this.arrFilter.length
+            // console.log(this.avgBaseline)
 
             this.pushValue=0
             for (var i=0; i<this.arrFilter.length; i++){
                 // if(this.arrFilter[i]>2500){
-                if(this.arrFilter[i]-this.baseLine2>200){ // check peak data with base line
+                if(this.arrFilter[i]-this.avgBaseline>200){ // check peak data with base line
                     //check data(value) drop
                     if(this.arrFilter[i]-this.arrFilter[i+2]>600){
                         //check data(value) before&after peak <peak
                         if(this.arrFilter[i]-this.arrFilter[i+1]>0&&this.arrFilter[i]-this.arrFilter[i-1]>0){
                             this.dataFilter[this.pushValue]=this.arrTimeFilter[i]
-                            this.pushValue+=1                               
+                            this.pushValue+=1     
+                            // console.log(this.pushValue)                          
                         }
                          
                     }
@@ -77,19 +85,28 @@ new Vue({
             this.sum = 0
             this.mean = 0
             this.square = 0
-            this.count = this.dataFilter.length - 1
+            this.count = this.dataFilter.length-1
+            // console.log(this.count)
+            for (var i=0; i<this.dataFilter.length; i++){
+                this.diffTime = this.dataFilter[i]-this.dataFilter[0]
+            }
+            this.heartRate=((this.diffTime/(this.pushValue-1))/1000)*60
+            this.heartRate=this.heartRate.toFixed(0) 
+            console.log(this.heartRate)
+            // console.log(this.diffTime)
+            // console.log(this.pushValue)
 
             for (var i=0; i<this.count; i++){
-                this.minus2[i] = this.dataFilter[i+1] - this.dataFilter[i]     //time2-time1 
+                this.minus[i] = this.dataFilter[i+1] - this.dataFilter[i]     //time2-time1 
             }
-            this.pushValue=0
-            for(var i=0; i<this.minus2.length; i++){
-                if(this.minus2[i]>100){
-                        this.minus[this.pushValue] = this.minus2[i]
-                        this.pushValue+=1
-                        // console.log(this.minus2[i]+'#'+i)
-                    }
-            }
+            // this.pushValue=0
+            // for(var i=0; i<this.minus2.length; i++){
+            //     if(this.minus2[i]>100){
+            //             this.minus[this.pushValue] = this.minus2[i]
+            //             this.pushValue+=1
+            //             // console.log(this.minus2[i]+'#'+i)
+            //         }
+            // }
             // console.log(this.minus)    
             for(var i=0;i<this.minus.length-1;i++){
                 this.RR[i] = this.minus[i+1] - this.minus[i]    //RR2-RR1
@@ -109,6 +126,7 @@ new Vue({
                 this.total = this.total.toFixed(2)                  //fix dot number
             // console.log(this.sum)
         },
+
         chart:function(){
             var ctx = document.getElementById('myChart').getContext('2d');
             var chart = new Chart(ctx, {
@@ -116,14 +134,14 @@ new Vue({
             // The data for our dataset
             
             data: {
-                labels:[] = this.times2,
+                labels:[] = this.times,
                 datasets: [{
                     label: 'ECG values',
                     backgroundColor: 'rgba(0, 0, 0, 0)',
                     lineTension:'0.3',
                     pointBorderColor:'',
                     borderColor: 'rgb(255, 99, 132)',
-                    data:[] = this.values2
+                    data:[] = this.values
                 }]
                 
             },
@@ -180,16 +198,17 @@ new Vue({
 
     created(){
         // show data from database
-        messageRef2.on('child_added',snapshot=>{
-            // push data from database to array
-            this.values2.push(snapshot.val().value)
-            this.times2.push(snapshot.val().time)
-            this.chart();
-        })
+        // messageRef2.on('child_added',snapshot=>{
+        //     // push data from database to array
+        //     this.values2.push(snapshot.val().value)
+        //     this.times2.push(snapshot.val().time)
+        //     this.chart();
+        // })
         messageRef.on('child_added' ,snapshot=>{
             // push data from database to array
             this.values.push(snapshot.val().value)
             this.times.push(snapshot.val().time)
+            this.chart();
             //console.log(snapshot.val().time); 
 
             // if(this.maxValue <  snapshot.val().value){
@@ -214,6 +233,7 @@ new Vue({
             // }
             this.calculate(); 
             this.filter();
+            // this.heartBeat();
             // this.chart();
             
         }) 
